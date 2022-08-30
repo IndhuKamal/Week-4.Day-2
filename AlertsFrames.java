a8737ebeedf0e5e6c1b823360426ee94201a872f
IndhuKamal <107314957+IndhuKamal@users.noreply.github.com>
2022-08-30 17:43:35 +0800

    Add files via upload

---
+++ AlertsFrames.java
@@ -0,0 +1,33 @@
+package week4.Day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AlertsFrames {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm");
+		driver.switchTo().frame("iframeResult");
+		driver.findElement(By.xpath("//button[text()='Try it']")).click();
+		Alert alert = driver.switchTo().alert();
+		alert.accept();
+		String text = driver.findElement(By.id("demo")).getText();
+		System.out.println(text);
+		if (text.contains("OK")) {
+			System.out.println("Accepted the Alert");
+			} else {
+            System.out.println("Pressed the Cancel");
+		}
+        
+	}
+
+}
